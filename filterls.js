9e5830780a9009af541ae236b322d6b723192503
zarathustrus <mujhekhatlikho@gmail.com>
2015-02-02 21:20:19 +0530

    filterlist solution

---
+++ filterls.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+var path = require('path');
+
+fs.readdir(process.argv[2], function doneReading(err, files){
+    for(i=0; i<files.length; i++){
+	if(path.extname(files[i])=== '.' + process.argv[3]){
+	    console.log(files[i])
+	}
+    }});
